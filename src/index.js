51ba3e018d223a818a89dc03802ac829ce938180
André Firmino <andredd780@gmail.com>
2024-10-15 23:54:34 -0300

    initialize api

---
+++ src/index.js
@@ -0,0 +1,32 @@
+const express = require("express"); // Importa o 'express' pro projeto
+const mongoose = require("mongoose"); // Importa o banco de dados 'mongoose'
+
+const app = express(); // Cria o nosso 'server/app' com o express
+app.use(express.json()); // Faz com que o servidor consiga receber requisições JSON
+
+const Person = mongoose.model("Person", {
+  nome: String,
+});
+
+const port = 3000; // Instala uma porta no nosso servidor
+
+app.get("/", async (req, res) => {
+  const allPerson = await Person.find();
+  res.send(allPerson);
+}); // Lista tudo
+
+app.post("/", async (req, res) => {
+  const person = new Person({
+    nome: req.body.nome,
+  });
+
+  await person.save();
+  res.send(person);
+}); // Enviando dados para o servidor
+
+app.listen(port, () => {
+  mongoose.connect(
+    "mongodb+srv://firminosteam:7o7m5TUXtXBY45SR@api-videos-nodejs.kctx0.mongodb.net/?retryWrites=true&w=majority&appName=api-videos-nodejs"
+  );
+  console.log(`Server is Running in ${port}`);
+});
